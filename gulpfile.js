bc11a6dfad3ba8b709cbdcca4c33e5fe60d119f6
Xanaxx999 <pedromelotessmann@gmail.com>
2026-02-16 11:35:18 -0300

    arrumando_burrice_MUITAS

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,15 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
 
+function html() {
+    return gulp.src("./src/**/*.html")
+        .pipe(gulp.dest("./dist"));
+}
+
 function styles() {
     return gulp.src("./src/styles/main.scss")
         .pipe(sass({outputStyle: "compressed"}))
         .pipe(gulp.dest("./dist/styles"));
-
 }
 
 function images() {
@@ -15,8 +19,10 @@ function images() {
         .pipe(gulp.dest("./dist/images"));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, html);
+
 exports.watch = function () {
     gulp.watch("./src/styles/**/*.scss", styles);
     gulp.watch("./src/images/**/*", images);
+    gulp.watch("./src/**/*.html", html);
 };
